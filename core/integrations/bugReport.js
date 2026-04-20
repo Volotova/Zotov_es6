7c984895b15bec8583283297d27469a2cc89e61c
Беляев Александр <118131020+Volotova@users.noreply.github.com>
2026-04-20 07:54:09 +0300

    Add files via upload

--- core/integrations/bugReport.js
+++ core/integrations/bugReport.js
@@ -1,1 +1,52 @@
+import axios from "axios";
 
+function botInfoForBugReport() {
+    var botData = {
+        projectName: $.injector.project,
+        botId: $.request.botId,
+        externalId: $.session.currentUserId || "default",
+        phone: Utils.getPhone()
+    };
+    return botData;
+}
+
+global.sendBugReport = async function (errorType, requestTime, responseFromBot, requestData) {
+    const botData = botInfoForBugReport();
+    // Форматируем время по московскому часовому поясу (UTC+3)
+    const requestTimeFormatted = moment.utc(requestTime).utcOffset("+03:00").format("ddd, DD MMM YYYY HH:mm:ss");
+    const clientId = "test";
+    const query = errorType;
+
+    const responseWithError = {
+        ...responseFromBot,
+        error: responseFromBot?.errorMessage || "No error message available"
+    };
+
+    const data = {
+        botData,
+        requestTime: requestTimeFormatted,
+        requestData,
+        response: responseWithError
+    };
+
+    try {
+        const secret = Utils.getSecret("BUG_REPORT");
+        const url = `${secret.url}/?clientId=${clientId}&query=${encodeURIComponent(query)}`;
+        const { data: responseData } = await axios.post(url, {
+            clientId,
+            query,
+            data
+        }, {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        return responseData;
+    } catch (error) {
+        Logger.warn(`[sendBugReport] Exception: ${error}`);
+        return false;
+    }
+}
+
+export default {}
--- core/integrations/chatApi.js
+++ core/integrations/chatApi.js
@@ -1,1 +1,11 @@
+global.chatApiRequest = async function(botURL, query = "query", clientId = "PMI_BOT") {
+    const url = `${botURL}?clientId=${clientId}&query=${encodeURIComponent(query)}`
 
+    const http = new HttpClient()
+        .init(undefined, "ChatAPI", 2)
+        .installMask("token");
+
+    return await http.get(url);
+}
+
+export default {}
